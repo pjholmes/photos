9364ba9be4c54babd73d60923067c6a15bdab9c2
Patrick Holmes <patrick.j.holmes@gmail.com>
2014-12-14 11:49:43 -0800

    refactored image scanner. Added Photo class

---
+++ src/main/scala/ImageScanner.scala
@@ -0,0 +1,68 @@
+/*
+Copyright 2014 Patrick Holmes
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+
+package com.github.pjholmes.photos
+
+import java.io.IOException
+import java.nio.file.attribute.BasicFileAttributes
+import java.nio.file._
+
+import grizzled.slf4j.Logger
+
+import scala.collection.mutable.ArrayBuffer
+
+object ImageScanner {
+  val logger = Logger("ImageScanner")
+
+  val exts = List(".jpg", ".png", ".bmp")
+
+  def isImageFile(fileName: String) = ImageScanner.exts.contains(fileName.takeRight(4).toLowerCase)
+
+  def shouldSkipDir(dir: String) = dir.endsWith(".photolibrary")
+
+  def getImageFilesBelowPath(path: String) = {
+    val files = ArrayBuffer.empty[String]
+    class Visitor extends SimpleFileVisitor[Path]
+    {
+      override def visitFile(file: Path, attr: BasicFileAttributes) : FileVisitResult = {
+        if (attr.isRegularFile && isImageFile(file.toString))
+          files += file.toString
+        FileVisitResult.CONTINUE
+      }
+      override def visitFileFailed(file: Path, ex: IOException) : FileVisitResult = {
+        logger.error(s"Exception: $file $ex")
+        FileVisitResult.CONTINUE
+      }
+      override def preVisitDirectory(dir: Path, attr: BasicFileAttributes) : FileVisitResult = {
+        if (shouldSkipDir(dir.toString))
+          FileVisitResult.SKIP_SUBTREE
+        else {
+          logger.debug(s"Entering: $dir")
+          FileVisitResult.CONTINUE
+        }
+      }
+    }
+    Files.walkFileTree(Paths.get(path), new Visitor)
+    files
+  }
+}
+
+
+
+
+
+
+
